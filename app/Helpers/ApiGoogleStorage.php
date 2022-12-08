7d2153e209dcce7b02cd0f605516727aab3116a1
Janou94 <78268572+Janou94@users.noreply.github.com>
2022-12-08 16:52:19 +0100

    Update ApiGoogleStorage.php

--- app/Helpers/ApiGoogleStorage.php
+++ app/Helpers/ApiGoogleStorage.php
@@ -45,7 +45,7 @@ class ApiGoogleStorage extends Model
             $productId=$row['productId'];
             $csv[] = [$productId, $min_price, $date];
         }
-        Log::info('CSV Entriez : '.sizeof($csv));
+        Log::info('CSV size for '.$merchant_id.$country_code.' : '.sizeof($csv));
         return $csv;
     }
 }
